1fb2b3274340a1bf71d9d80f8b1a2cfd0e57542d
EternallyHyper <coder2582@gmail.com>
2026-09-24 19:36:34 -0400

    fixes

--- html/main.js
+++ html/main.js
@@ -918,7 +918,7 @@ function getNewsPages() {
       ],
       changes: [
         { text: "Hotfixes", desc: "Made the default theme a little bit better" },
-        { text: "Games Update", desc: "Added suggestions for y'all to suggest me games :D" },
+        { text: "Huge Games Update", desc: "Fixed Escape Road, added Escape Road 2 + gn-math and suggestions for y'all to suggest me games :D" },
         { text: "Update", desc: "expect mini updates soon lol as I add stuff :D" }
       ]
     },
