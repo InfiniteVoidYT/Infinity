271c4a0a033e38cb93f2608640486e14e12c7274
EternallyHyper <coder2582@gmail.com>
2026-09-25 18:49:49 -0400

    until i find a proper fix no more escape road :p

--- html/main.js
+++ html/main.js
@@ -918,7 +918,6 @@ function getNewsPages() {
       ],
       changes: [
         { text: "Hotfixes", desc: "Made the default theme a little bit better" },
-        { text: "Huge Games Update", desc: "Fixed Escape Road, added Escape Road 2 + gn-math and suggestions for y'all to suggest me games :D" },
         { text: "Update", desc: "expect mini updates soon lol as I add stuff :D" }
       ]
     },
